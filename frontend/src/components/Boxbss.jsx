0e0194ee5465449ec07322ab8ebf04c882e4d5c2
AakritiMeh <aakriti.meh.2004@gmail.com>
2024-12-18 18:46:11 +0530

    sponsors page base and box component added

---
+++ frontend/src/components/Boxbss.jsx
@@ -0,0 +1,12 @@
+import sp from "../assets/sp.png"
+import "../styles/boxbs.css"
+const Boxbss= () => {
+  return (
+    <div className="BoxBS">
+        <img height="125" width="125" src={sp} ></img>
+
+    </div>
+  )
+}
+
+export default Boxbss
---
+++ frontend/src/pages/Sponsors.jsx
@@ -0,0 +1,39 @@
+import "../styles/sponsors.css";
+import BoxBS from "../components/Boxbss";
+
+const Sponsors = () => {
+  return (
+    <div className="SponsorsMain">
+      <div className="HeadingSpon">Our Sponsors</div>
+      <div className="allSponsors">
+        <div className="sponsorRow sponsorRow1">
+          <BoxBS />
+          <BoxBS />
+          <BoxBS />
+          <BoxBS />
+          <BoxBS />
+          <BoxBS />
+        </div>
+        <div className="sponsorRow sponsorRow2">
+          <BoxBS />
+          <BoxBS />
+          <BoxBS />
+          <BoxBS />
+          <BoxBS />
+          <BoxBS />
+        </div>
+        <div className="sponsorRow sponsorRow3">
+          <BoxBS />
+          <BoxBS />
+          <BoxBS />
+          <BoxBS />
+          <BoxBS />
+          <BoxBS />
+        
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sponsors;
