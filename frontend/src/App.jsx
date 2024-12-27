15b4f6f6e56380fb2b672aaa24fa7b30f31bddac
V.Prasanth <prasanth.v2023@vitstudent.ac.in>
2024-12-27 19:10:05 +0530

    MILESTONES

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import BlogSlider from './components/BlogSection';
 import Clients from './components/Clients';
 // import Boxbss from './components/Boxbss'
 import "./components/MemberCard";
+import Milestone from './components/Milestone';
+
 import "./pages/Members"
 import Members from './pages/Members';
 function App() {
@@ -14,6 +16,8 @@ function App() {
       <Members />
       {/* <BlogSlider/> */}
       <Clients />
+      <Milestone />
+      
   
      
     </>
--- frontend/src/data/index.js
+++ frontend/src/data/index.js
@@ -30,4 +30,65 @@ export const testimonials = [
       title: "Co-founder of Elevate Digital",
     },
   ];
+
+  export const  experiences  = [
+    {
+      id: 0,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+      role: "DevOps & Fullstack Engineering Intern",
+      company: "Flipr Inovations Pvt. Ltd.",
+      date: "Aug 2023 - July 2023",
+      desc: "Working on Flipr Platforms, managing DevOps, and streamlining the process with automation.",
+    },
+    {
+      id: 1,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+      role: "Fullstack Externship",
+      company: "Flipr",
+      date: "June 2023 - July 2023",
+      desc: "Built an employee management full stack web app used Docker and deployed on AWS ec2. I was the top performer in the program.",
+     
+    },
+    
+  ];
+
+
+  export const milestones = [
+    {
+      id: 0,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+      milestone: "DevOps & Fullstack Engineering Internship",
+      organization: "Cyberteam",
+      date: "Aug 2023 - July 2023",
+      details: "Enhanced Cyberteam's platforms by managing DevOps operations and introducing process automation for efficiency.",
+    },
+    {
+      id: 1,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+      milestone: "Fullstack Development Externship",
+      organization: "Cyberteam",
+      date: "June 2023 - July 2023",
+      details: "Developed an employee management web app using full stack technologies, implemented Docker, and deployed it on AWS EC2. Achieved recognition as a top performer in the program.",
+    },
+    {
+      id: 2,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+      milestone: "Cloud Infrastructure Automation Project",
+      organization: "Cyberteam",
+      date: "March 2023 - May 2023",
+      details: "Spearheaded a project to automate cloud infrastructure provisioning using Terraform, significantly reducing deployment times and increasing scalability.",
+    },
+    {
+      id: 3,
+      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+      milestone: "Cybersecurity Analysis Initiative",
+      organization: "Cyberteam",
+      date: "Jan 2023 - Feb 2023",
+      details: "Conducted a comprehensive analysis of system vulnerabilities and implemented security protocols, reducing security risks by 40%.",
+    },
+];
+
+  
+  
+  
   
