69ab0b01fa0e9a6b4bf15265cb24a7bc5ec9fbe0
V.Prasanth <prasanth.v2023@vitstudent.ac.in>
2025-02-12 13:01:06 +0530

    footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,8 @@ import './App.css'
 import BlogSlider from './components/BlogSection';
 
 import FAQSection from './components/Faqs';
-import FooterSection from './components/Footer';
+import Footer from './components/Footer';
+
 import { SpotlightNewDemo } from './components/Hero';
 // import Boxbss from './components/Boxbss'
 import "./components/MemberCard";
@@ -27,33 +28,7 @@ function App() {
     
     <div className="bg-gray-900 text-white min-h-screen">
       <Navbar />
-      {/* <BrowserRouter>
-      <Routes>
-        <Route exact path="" element={<SpotlightNewDemo />}>
-        
-        </Route>
-        <Route
-          path="/members"
-          element={<Members />}
-        >
-
-        </Route>
-        <Route
-          path="/blogs"
-          element={<BlogSlider />}
-
-        >
-
-        </Route>
-        <Route
-          path="/milestones"
-          element={<Milestone />}
-          
-        >
-
-        </Route>
-      </Routes>
-      </BrowserRouter> */}
+      
       <SpotlightNewDemo />
       <Members />
       <BlogSlider />
@@ -67,7 +42,8 @@ function App() {
       <FAQSection />
       <SVGMaskEffectDemo />
       <Sponsors />
-      <FooterSection />
+      <Footer />
+      
      
     </div>
 
